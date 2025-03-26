99e7f4ad147ff194e852d836d952dd73321d1c9c
lianleyenda <lianleyende24@gmail.com>
2025-03-26 12:10:52 -0300

    APRENDIMOS A USAR EL INT Y STRING

--- app.py
+++ app.py
@@ -16,9 +16,28 @@ def hello_world():
 def COMIDA():
     return "<h2>amo las hamburgeesas</h2>"
 
-
+@app.route("/HAMBUERGUESA/<string:condimentos>")
+def COMIDA_condimentos(condimentos):
+    return f"<h2>amo las hamburgeesas {condimentos}</h2>"
 
 @app.route("/papas_fritas")
 def acompañamiento():
     return "<h2>papas fritas</h2>"
+
+
+
+@app.route("/dados/<int:caras>")
+def dado(caras):
+    from random import randint
+    numero = randint(1,caras)
+    return f"<h2>dado de {caras}, salio {numero}!</h2>"
+
+
+
+@app.route("/sumar/<int:n1>/<int:n2>")
+def suma(n1,n2):
+    suma = n1+n2
+    return f"<h2>{n1} mas {n2} da {suma}</h2>"
+
+
 
