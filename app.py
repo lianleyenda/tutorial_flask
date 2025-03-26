051fa35368cab0fbbc31d6201be27c0d7e5debef
lianleyenda <lianleyende24@gmail.com>
2025-03-26 11:46:54 -0300

    AGREGAMOS LINK CON OTRAS DIRECCIONES

--- app.py
+++ app.py
@@ -4,4 +4,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return """
+       <a href='/HAMBUERGUESA'>HAMBUERGUESA</a>
+       <a href='/papas_fritas'>papas_fritas</a>
+       
+
+"""
+
+
+@app.route("/HAMBUERGUESA")
+def COMIDA():
+    return "<h2>amo las hamburgeesas</h2>"
+
+
+
+@app.route("/papas_fritas")
+def acompañamiento():
+    return "<h2>papas fritas</h2>"
+
